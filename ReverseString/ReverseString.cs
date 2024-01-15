b504a3088923ec7c74f88f02af98795ce33b9c6a
G Locke <jkeys726@gmail.com>
2024-01-15 11:58:18 -0600

    initial commit

---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿List<String> inputs = new List<string>();
+string? line;
+Console.WriteLine("Enter lines of text (type 'END' to finish): ");
+
+while ((line = Console.ReadLine()) != "END")
+{
+    if (line != null) {
+        inputs.Add(line);
+    } 
+}
+Console.WriteLine("Here's your input backwards");
+for (int i = inputs.Count - 1; i >= 0; i--) {
+    Console.WriteLine(ReverseString(inputs[i]));
+}
+
+static string ReverseString(string input)
+{
+    char[] characterArray = input.ToCharArray();
+    string reversedString = String.Empty;
+    for (int i = characterArray.Length - 1; i >= 0; i--)
+    {
+        reversedString += characterArray[i];
+    }
+
+    return reversedString;
+}
